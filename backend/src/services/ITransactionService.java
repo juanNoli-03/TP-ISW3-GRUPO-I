2393b96817b9ea44c44f9c83ba4f258545bf0ea2
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-04-30 14:10:17 -0300

    Definición de services

---
+++ backend/src/services/ITransactionService.java
@@ -0,0 +1,97 @@
+package services;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import datos.Transaction;
+
+///Interface ITransactionService:
+public interface ITransactionService {
+	///Encontrar:
+	
+	//Encontramos la transacción con determinado id:
+	public Transaction findByIdTransaction(int idTransaction);
+	
+	//Encontramos las transacciones en determinada fecha:
+	public List<Transaction> findByDate(LocalDate date);
+	
+	//Encontramos las transacciones en determinada fecha o anteriores a ella:
+	public List<Transaction> findByDateBeforeThanOrEqualTo(LocalDate date);
+	
+	//Encontramos las transacciones en determinada fecha o posteriores a ella:
+	public List<Transaction> findByDateAfterThanOrEqualTo(LocalDate date);
+	
+	//Encontramos las transacciones con una fecha en un rango determinado (extremos incluidos):
+	public List<Transaction> findByDateRange(LocalDate fromDate, LocalDate untilDate);
+	
+	//Encontramos las transacciones en determinada hora:
+	public List<Transaction> findByTime(LocalTime time);
+	
+	//Encontramos las transacciones en determinada hora o anteriores a ella:
+	public List<Transaction> findByTimeBeforeThanOrEqualTo(LocalTime time);
+	
+	//Encontramos las transacciones en determinada hora o posteriores a ella:
+	public List<Transaction> findByTimeAfterThanOrEqualTo(LocalTime time);
+	
+	//Encontramos las transacciones con una hora en un rango determinado (extremos incluidos):
+	public List<Transaction> findByTimeRange(LocalTime fromTime, LocalTime untilTime);
+	
+	//Encontramos las transacciones en determinada fecha y hora:
+	public List<Transaction> findByDateAndTime(LocalDate date, LocalTime time);
+	
+	//Encontramos las transacciones de determinado tipo:
+	public List<Transaction> findByType(String type);
+	
+	//Encontramos las transacciones con un precio menor o igual a uno determinado:
+	public List<Transaction> findByPriceLessThanOrEqualTo(float price);
+	
+	//Encontramos las transacciones con un precio mayor o igual a uno determinado:
+	public List<Transaction> findByPriceGreaterThanOrEqualTo(float price);
+	
+	//Encontramos las transacciones con un precio en un rango determinado (extremos incluidos):
+	public List<Transaction> findByPriceRange(float minimumPrice, float maximumPrice);
+	
+	//Encontramos las transacciones de determinada tarjeta:
+	public List<Transaction> findByCard(int idCard);
+	
+	///Obtener:
+	
+	//Obtenemos todas las transacciones:
+	public List<Transaction> getAll();
+	
+	///Ordenar:
+	
+	//Obtenemos todas las transacciones ordenadas por fecha de forma ascendente:
+	public List<Transaction> getAllInOrderAscByDate();
+	
+	//Obtenemos todas las transacciones ordenadas por fecha de forma descendente:
+	public List<Transaction> getAllInOrderDescByDate();
+	
+	//Obtenemos todas las transacciones ordenadas por hora de forma ascendente:
+	public List<Transaction> getAllInOrderAscByTime();
+	
+	//Obtenemos todas las transacciones ordenadas por hora de forma descendente:
+	public List<Transaction> getAllInOrderDescByTime();
+	
+	//Obtenemos todas las transacciones ordenadas por precio de forma ascendente:
+	public List<Transaction> getAllInOrderAscByPrice();
+	
+	//Obtenemos todas las transacciones ordenadas por precio de forma descendente:
+	public List<Transaction> getAllInOrderDescByPrice();
+	
+	///Agregar:
+	
+	//Agregamos una transacción:
+	public Transaction insert(Transaction transaction) throws Exception;
+	
+	///Modificar:
+	
+	//Modificamos una transacción:
+	public Transaction update(Transaction transaction) throws Exception;
+	
+	///Eliminar:
+	
+	//Eliminamos físicamente una transacción:
+	public boolean remove(int idTransaction);
+}
---
+++ backend/src/services/IUserService.java
@@ -0,0 +1,62 @@
+package services;
+
+import java.util.List;
+
+import datos.User;
+
+///Interface IUserService:
+public interface IUserService {
+	///Encontrar:
+	
+	//Encontramos el usuario con determinado id:
+	public User findByIdUser(int idUser);
+	
+	//Encontramos el usuario con determinado email:
+	public User findByEmail(String email);
+	
+	//Encontramos el usuario con determinado DNI:
+	public User findByDni(int dni);
+	
+	//Encontramos los usuarios con determinado nombre:
+	public List<User> findByName(String name);
+	
+	///Obtener:
+	
+	//Obtenemos todos los usuarios:
+	public List<User> getAll();
+	
+	///Ordenar:
+	
+	//Obtenemos todos los usuarios ordenados por nombre de forma ascendente:
+	public List<User> getAllInOrderAscByName();
+	
+	//Obtenemos todos los usuarios ordenados por nombre de forma descendente:
+	public List<User> getAllInOrderDescByName();
+	
+	//Obtenemos todos los usuarios ordenados por email de forma ascendente:
+	public List<User> getAllInOrderAscByEmail();
+	
+	//Obtenemos todos los usuarios ordenados por email de forma descendente:
+	public List<User> getAllInOrderDescByEmail();
+	
+	//Obtenemos todos los usuarios ordenados por DNI de forma ascendente:
+	public List<User> getAllInOrderAscByDni();
+	
+	//Obtenemos todos los usuarios ordenados por DNI de forma descendente:
+	public List<User> getAllInOrderDescByDni();
+	
+	///Agregar:
+	
+	//Agregamos un usuario:
+	public User insert(User user) throws Exception;
+	
+	///Modificar:
+	
+	//Modificamos un usuario:
+	public User update(User user) throws Exception;
+	
+	///Eliminar:
+	
+	//Eliminamos físicamente un usuario:
+	public boolean remove(int idUser);
+}
