a602c6834dc4454debadcc3b512ff53eb134097f
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-05-27 18:51:44 -0300

    Update SCRIPT CREACION DE TABLAS.sql

--- SCRIPT CREACION DE TABLAS.sql
+++ SCRIPT CREACION DE TABLAS.sql
@@ -15,88 +15,91 @@ CREATE SCHEMA IF NOT EXISTS `DER-ISW3` DEFAULT CHARACTER SET utf8 ;
 USE `DER-ISW3` ;
 
 -- -----------------------------------------------------
--- Table `DER-ISW3`.`user`
+-- Table `DER-ISW3`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `DER-ISW3`.`user` (
-  `id_user` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `DER-ISW3`.`users` (
+  `users_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `dni` INT NOT NULL,
-  PRIMARY KEY (`id_user`),
+  PRIMARY KEY (`users_id`),
   UNIQUE INDEX `dni_UNIQUE` (`dni` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `DER-ISW3`.`card`
+-- Table `DER-ISW3`.`cards`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `DER-ISW3`.`card` (
-  `id_card` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `DER-ISW3`.`cards` (
+  `cards_id` INT NOT NULL AUTO_INCREMENT,
   `number` INT NOT NULL,
   `balance` FLOAT NOT NULL,
   `activated` TINYINT NOT NULL,
-  `user_id_user` INT NOT NULL,
-  PRIMARY KEY (`id_card`),
+  `users_id` INT NOT NULL,
+  PRIMARY KEY (`cards_id`),
   UNIQUE INDEX `number_UNIQUE` (`number` ASC) VISIBLE,
-  INDEX `fk_card_user1_idx` (`user_id_user` ASC) VISIBLE,
+  INDEX `fk_card_user1_idx` (`users_id` ASC) VISIBLE,
   CONSTRAINT `fk_card_user1`
-    FOREIGN KEY (`user_id_user`)
-    REFERENCES `DER-ISW3`.`user` (`id_user`)
+    FOREIGN KEY (`users_id`)
+    REFERENCES `DER-ISW3`.`users` (`users_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `DER-ISW3`.`transaction`
+-- Table `DER-ISW3`.`transactions`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `DER-ISW3`.`transaction` (
-  `id_transaction` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `DER-ISW3`.`transactions` (
+  `transactions_id` INT NOT NULL AUTO_INCREMENT,
   `date` DATE NOT NULL,
   `time` TIME NOT NULL,
   `type` VARCHAR(45) NOT NULL,
   `description` VARCHAR(150) NOT NULL,
   `price` FLOAT NOT NULL,
-  `card_id_card` INT NOT NULL,
-  PRIMARY KEY (`id_transaction`),
-  INDEX `fk_transaction_card1_idx` (`card_id_card` ASC) VISIBLE,
+  `cards_id` INT NOT NULL,
+  PRIMARY KEY (`transactions_id`),
+  INDEX `fk_transaction_card1_idx` (`cards_id` ASC) VISIBLE,
   CONSTRAINT `fk_transaction_card1`
-    FOREIGN KEY (`card_id_card`)
-    REFERENCES `DER-ISW3`.`card` (`id_card`)
+    FOREIGN KEY (`cards_id`)
+    REFERENCES `DER-ISW3`.`cards` (`cards_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `DER-ISW3`.`payment method`
+-- Table `DER-ISW3`.`payment_methods`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `DER-ISW3`.`payment method` (
-  `id_payment_method` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `DER-ISW3`.`payment_methods` (
+  `payment_methods_id` INT NOT NULL AUTO_INCREMENT,
   `type` VARCHAR(45) NOT NULL,
   `activated` TINYINT NOT NULL,
   `data` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_payment_method`))
+  PRIMARY KEY (`payment_methods_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `DER-ISW3`.`user x payment method`
+-- Table `DER-ISW3`.`users_x_payment_methods`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `DER-ISW3`.`user x payment method` (
-  `user_id_user` INT NOT NULL,
-  `payment method_id_payment_method` INT NOT NULL,
-  INDEX `fk_user x payment method_user_idx` (`user_id_user` ASC) VISIBLE,
-  INDEX `fk_user x payment method_payment method1_idx` (`payment method_id_payment_method` ASC) VISIBLE,
+CREATE TABLE IF NOT EXISTS `DER-ISW3`.`users_x_payment_methods` (
+  `users_id` INT NOT NULL,
+  `payment_methods_id` INT NOT NULL,
+  `data` VARCHAR(45) NOT NULL,
+  `activated` TINYINT NOT NULL,
+  INDEX `fk_user x payment method_user_idx` (`users_id` ASC) VISIBLE,
+  INDEX `fk_user x payment method_payment method1_idx` (`payment_methods_id` ASC) VISIBLE,
+  PRIMARY KEY (`users_id`, `payment_methods_id`),
   CONSTRAINT `fk_user x payment method_user`
-    FOREIGN KEY (`user_id_user`)
-    REFERENCES `DER-ISW3`.`user` (`id_user`)
+    FOREIGN KEY (`users_id`)
+    REFERENCES `DER-ISW3`.`users` (`users_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_user x payment method_payment method1`
-    FOREIGN KEY (`payment method_id_payment_method`)
-    REFERENCES `DER-ISW3`.`payment method` (`id_payment_method`)
+    FOREIGN KEY (`payment_methods_id`)
+    REFERENCES `DER-ISW3`.`payment_methods` (`payment_methods_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
