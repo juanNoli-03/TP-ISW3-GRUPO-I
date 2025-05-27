114c8b062a95b911f2bc5465e2072d299239f98a
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-05-27 18:05:35 -0300

    Datos iniciales del sistema

---
+++ frontend/src/data/payment_methods.js
@@ -0,0 +1,8 @@
+//Métodos de pago registrados:
+export let paymentMethods = [
+  { id: 1, type: 'Débito' },
+  { id: 2, type: 'QR' },
+  { id: 3, type: 'Cuenta DNI' },
+  { id: 4, type: 'Mercado Pago' },
+  { id: 5, type: 'Ualá' }
+]
